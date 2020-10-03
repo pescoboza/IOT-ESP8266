bed470c9d141a2bb4eadd907659c10e15ad2749b
Pedro Escoboza <lolo23jhon@gmail.com>
2020-10-03 16:48:00 -0700

    :wrench: Moved magic numbers to config.h.

--- src/main.cpp
+++ src/main.cpp
@@ -2,88 +2,73 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-#include "secret.h"
+#include "config.h"
 
 #define BUFF_SIZE 255
+unsigned long lastTime{0U};
 
-using ul = unsigned long;
+void setup(){
+    pinMode(LED_BUILTIN, OUTPUT);
+    
+    Serial.begin(cfg::BAUD_RATE);
+    delay(cfg::INITIAL_DELAY_MS);
+    WiFi.begin(cfg::SSID, cfg::PASSWORD);
 
-const char *SSID{secret::SSID};
-const char *PASSWORD{secret::PASSWORD};
-const char *SERVER_NAME{"http://192.168.56.1:5000/update-sensor"};
-int SENSOR_ID{1};
+    // Print SSID
+    Serial.print("Connecting to WiFi network with the SSID: ");
+    Serial.println(cfg::SSID);
+    while (WiFi.status() != WL_CONNECTED){
+        delay(cfg::WIFI_CONNECT_DELAY_MS);
+        Serial.print(".");
+    }
+    Serial.println("");
 
-ul lastTime{0U};
-ul timerDelay{5000U};
+    // Print local IP
+    Serial.print("Local IP is: ");
+    Serial.println(WiFi.localIP());
 
-void setup()
-{
-  Serial.begin(9600);
-  delay(3000);
-  WiFi.begin(SSID, PASSWORD);
-
-  // Print SSID
-  Serial.print("Connecting to WiFi network with the SSID: ");
-  Serial.println(SSID);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  // Print local IP
-  Serial.print("Local IP is: ");
-  Serial.println(WiFi.localIP());
-
-  // Print initial reading delay
-  Serial.print("First reading delay set to ");
-  Serial.print(timerDelay);
-  Serial.println(" milliseconds.");
+    // Print initial reading delay
+    Serial.print("First reading delay set to ");
+    Serial.print(cfg::TIME_DELAY_MS);
+    Serial.println(" milliseconds.");
 }
 
-void loop()
-{
-  if ((millis() - lastTime) > timerDelay)
-  {
+void loop(){
+    if ((millis() - lastTime) > cfg::TIME_DELAY_MS){
 
-    if (WiFi.status() == WL_CONNECTION_LOST)
-    {
-      HTTPClient http;
+        if (WiFi.status() == WL_CONNECTION_LOST){
+            HTTPClient http;
 
-      http.begin(SERVER_NAME);
+            http.begin(cfg::SERVER_NAME);
 
-      // Content type header
-      http.addHeader("Content-Type", "application/json");
+            // Content type header
+            http.addHeader("Content-Type", "application/json");
 
-      // Format data to JSON
-      char buff[BUFF_SIZE];
-      const char *baseJsonStr{"{\"api_key\": \"%s\", \"sensor_id\": %i, \"value1\": %f}"};
-      sprintf(buff, baseJsonStr, secret::API_KEY, SENSOR_ID, 25.666f);
+            // Format data to JSON
+            char buff[BUFF_SIZE];
+            const char *baseJsonStr{"{\"api_key\": \"%s\", \"sensor_id\": %i, \"value1\": %f}"};
+            sprintf(buff, baseJsonStr, cfg::API_KEY, cfg::SENSOR_ID, 25.666f);
 
-      // Send HTTP POST request
-      int httpResponseCode{http.POST(buff)};
+            // Send HTTP POST request
+            int httpResponseCode{http.POST(buff)};
 
-      // Report back response
-      if (httpResponseCode > 0)
-      {
-        Serial.print("HTTP Reponse code: ");
-        Serial.println(httpResponseCode);
-        String payload{http.getString()};
-        Serial.println(payload);
-      }
-      else
-      {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
+            // Report back response
+            if (httpResponseCode > 0){
+                Serial.print("HTTP Reponse code: ");
+                Serial.println(httpResponseCode);
+                String payload{http.getString()};
+                Serial.println(payload);
+            }
+            else{
+                Serial.print("Error code: ");
+                Serial.println(httpResponseCode);
+            }
 
-      http.end();
+            http.end();
+        }
+        else{
+            Serial.println("WiFi Disconnected");
+        }
+        lastTime = millis();
     }
-    else
-    {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
 }
